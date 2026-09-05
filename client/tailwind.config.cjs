3e41f7abb2a868d125832e7cc13999cd89b21ff6
elinorgodzilla <106490429+elinorgodzilla@users.noreply.github.com>
2026-09-05 15:02:58 +0200

    feat: switch app typography to Readex Pro Light

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
   darkMode: ['class'],
   theme: {
     fontFamily: {
-      sans: ['Jost', 'Inter', 'sans-serif'],
+      sans: ['Readex Pro', 'Inter', 'sans-serif'],
       mono: ['Roboto Mono', 'monospace'],
     },
     // fontFamily: {
